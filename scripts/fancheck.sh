865fc97ad1f3cc5c5e6bdc41f6349ef4a9d63cb4
Tonguechaude <evan.challias@tonguechaude.fr>
2025-06-15 14:46:35 +0200

    fix : fancheck script

--- scripts/fancheck.sh
+++ scripts/fancheck.sh
@@ -2,13 +2,16 @@
 
 sensors_output=$(sensors)
 
-proc_rpm=$(echo "$sensors_output" | grep "Processor Fan" | sed -E 's/.*Processor Fan:\s*([0-9]+) RPM.*/\1/')
-mobo_rpm=$(echo "$sensors_output" | grep "Motherboard Fan" | sed -E 's/.*Motherboard Fan:\s*([0-9]+) RPM.*/\1/')
+proc_rpm=$(echo "$sensors_output" | grep -E "^cpu_fan:" | sed -E 's/.*cpu_fan:\s*([0-9]+) RPM.*/\1/')
+gpu_rpm=$(echo "$sensors_output" | grep -E "^gpu_fan:" | sed -E 's/.*gpu_fan:\s*([0-9]+) RPM.*/\1/')
+mid_rpm=$(echo "$sensors_output" | grep -E "^mid_fan:" | sed -E 's/.*mid_fan:\s*([0-9]+) RPM.*/\1/')
+
 
 proc_rpm=${proc_rpm:-0}
-mobo_rpm=${mobo_rpm:-0}
+gpu_rpm=${gpu_rpm:-0}
+mid_rpm=${mid_rpm:-0}
 
-if [[ $proc_rpm -gt 0 || $mobo_rpm -gt 0 ]]; then
+if [[ $proc_rpm -gt 0 || $gpu_rpm -gt 0 || $mid_rpm -gt 0 ]]; then
     status="on"
 else
     status="off"
@@ -16,7 +19,7 @@ fi
 
 jq --unbuffered --compact-output -n \
   --arg alt "$status" \
-  --arg tooltip "CPU Fan: ${proc_rpm} RPM&#10;GPU Fan: ${mobo_rpm} RPM" \
+  --arg tooltip "CPU Fan: ${proc_rpm} RPM&#10;GPU Fan: ${gpu_rpm} RPM; MID Fan: ${mid_rpm} RPM" \
   --arg class "$status" \
   '{
     alt: $alt,
