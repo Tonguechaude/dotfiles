4ac004ce1b10eabb1a2c28dcd041c8e491a9737b
Tonguechaude <evan.challias@tonguechaude.fr>
2025-09-23 20:18:04 +0200

    fix : lua config

--- nvim/.config/nvim/lua/enable_hardtime.lua
+++ nvim/.config/nvim/lua/enable_hardtime.lua
@@ -6,12 +6,16 @@ vim.notify = require("notify")
 -- Break bad habits, master Vim motions
 -- See plugins lua
 require("hardtime").setup({
-    -- I keep arrow keys because HJKL makes no sense with my layout
+    -- I keep arrow keys and HJKL because I'm addict
     disabled_keys = {
         ["<Up>"] = {},
         ["<Down>"] = {},
         ["<Left>"] = {},
         ["<Right>"] = {},
+        ["h"] = {},
+        ["j"] = {},
+        ["k"] = {},
+        ["l"] = {},
     },
 })
 vim.opt.termguicolors = true
