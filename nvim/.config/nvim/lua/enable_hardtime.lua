279d1b035b960b5cd5978fbf92676feaca643121
Tonguechaude <evan.challias@tonguechaude.fr>
2025-09-29 16:51:03 +0200

    enable HJKL keys

--- nvim/.config/nvim/lua/enable_hardtime.lua
+++ nvim/.config/nvim/lua/enable_hardtime.lua
@@ -6,16 +6,19 @@ vim.notify = require("notify")
 -- Break bad habits, master Vim motions
 -- See plugins lua
 require("hardtime").setup({
-    -- I keep arrow keys and HJKL because I'm addict
-    disabled_keys = {
-        ["<Up>"] = {},
-        ["<Down>"] = {},
-        ["<Left>"] = {},
-        ["<Right>"] = {},
-        ["h"] = {},
-        ["j"] = {},
-        ["k"] = {},
-        ["l"] = {},
-    },
+  -- I keep arrow keys and HJKL because I'm addict
+  disabled_keys = {
+    ["<Up>"] = {},
+    ["<Down>"] = {},
+    ["<Left>"] = {},
+    ["<Right>"] = {},
+  },
+
+  restricted_keys = {
+    ["h"] = {},
+    ["j"] = {},
+    ["k"] = {},
+    ["l"] = {},
+  }
 })
 vim.opt.termguicolors = true
